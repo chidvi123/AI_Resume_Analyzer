e9d343e32da2f77077286e293e5174fbe85bb0fb
Chidvilas Palarpa <palarpachidvilas2419@gmail.com>
2026-07-17 09:24:42 +0530

    chore: remove seed_admin.py from repo tracking

--- seed_admin.py
+++
@@ -1,8 +0,0 @@
-from backend.database.auth import register_user
-
-result = register_user("admin","admin123","admin")
-
-if result:
-    print("Admin user created Successfully")
-else:
-    print("Admin already exists")
